5e17ab3010c013de3fd088352f7005841d56ef10
KiranKamal Chauhan <chauhankirankamal@yahoo.com>
2020-05-01 21:07:49 +0530

    angular May 1 commit

--- discogApp/src/app/dis-finder.service.ts
+++ discogApp/src/app/dis-finder.service.ts
@@ -10,11 +10,13 @@ export class DisFinderService {
 
  consumerKey = 'fdcuhtjKYqnpoIrvAaQK';
  consumerSecret = 'UkMawUgGphTQmPjFIzfItIASxmwMjUvf';
- private api_url = "https://api.discogs.com/database/search?q=Nirvana&key=fdcuhtjKYqnpoIrvAaQK&secret=UkMawUgGphTQmPjFIzfItIASxmwMjUvf";
+ //private api_url = "https://api.discogs.com/database/search?q=Nirvana&key=fdcuhtjKYqnpoIrvAaQK&secret=UkMawUgGphTQmPjFIzfItIASxmwMjUvf";
+ private api_url = "https://api.discogs.com/database/search";
   
  getDicogs(searchParam) {
     //console.log(searchParam);
-    return this._http.get(this.api_url).
+   var url = this.api_url+'?q='+searchParam+'&key=fdcuhtjKYqnpoIrvAaQK&secret=UkMawUgGphTQmPjFIzfItIASxmwMjUvf';
+    return this._http.get(url).
     pipe(map(res => res));
   }
 }
